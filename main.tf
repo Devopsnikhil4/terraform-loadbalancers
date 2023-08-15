504d32f0d9eb0e65f44abac56de6941692f9bd7d
Devopsnikhil4 <138021468+Devopsnikhil4@users.noreply.github.com>
2023-08-15 20:08:43 +0530

    Auto Push

---
+++ main.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source                      = "./vendor/modules/vpc"
+  VPC_CIDR                    = var.VPC_CIDR  
+  ENV                         = var.ENV
+
+
+}
+
+
+# You cannot variablize this piece of code in the source parameter
